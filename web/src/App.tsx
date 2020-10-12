57e8ac493fa24f7cf95b451f5e9061bcfe926d05
marcelogaldino <marcelinhomgo@gmail.com>
2020-10-12 16:09:22 -0300

    Created landing page and routes

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,22 +1,13 @@
-import React from 'react';
+import React from 'react'
 
-interface TitleProps {
-  text: String
-}
+import './styles/global.css'
 
-function Title(props: TitleProps) {
-  return <h1>{props.text}</h1>
-}
+import Routes from './routes'
 
 function App() {
   return (
-    <div className="App">
-      <Title text="Hello Marcelo 1"/>
-      <Title text="Hello Marcelo 2"/>
-      <Title text="Hello Marcelo 3"/>
-      <Title text="Hello Marcelo 4"/>
-    </div>
-  );
+    <Routes />
+  )
 }
 
-export default App;
+export default App
---
+++ web/src/pages/OrphanagesMap.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function OrphanagesMap() {
+    return <h1>Orphanages Map</h1>
+}
+
+export default OrphanagesMap
