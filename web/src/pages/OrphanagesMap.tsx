fdaaedf939f3a7964e59d9749eeccbb6a1f53a53
Marcelo Galdino De Oliveira <marcelog@romagnole.com.br>
2020-10-14 11:37:41 -0300

    components abstractions included, config axios, pages create and show

--- web/src/pages/OrphanagesMap.tsx
+++ web/src/pages/OrphanagesMap.tsx
@@ -1,17 +1,28 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Link } from 'react-router-dom'
-import { Map, TileLayer } from 'react-leaflet'
+import { Map, TileLayer, Marker, Popup } from 'react-leaflet'
 
-import { FiPlus } from 'react-icons/fi'
+import { FiPlus, FiArrowRight } from 'react-icons/fi'
+
+import MapIcon from '../utils/mapIcon'
+
+import mapMarkerImg from '../images/map-marker.svg'
 
 import '../styles/pages/orphanages-map.css'
-import 'leaflet/dist/leaflet.css'
+import api from '../services/api'
 
 function OrphanagesMap() {
+    useEffect(() => {
+        api.get('orphanages').then(response => {
+            console.log(response)
+        })
+    }, [])
+
     return (
         <div id="page-map">
             <aside>
                 <header>
+                    <img src={mapMarkerImg} alt="Happy"/>
                     <h2>Escolha um orfanato no mapa</h2>
                     <p>Muitas crianças estão esperando a sua visita :)</p>
                 </header>
@@ -28,9 +39,23 @@ function OrphanagesMap() {
                 style={{width: '100%', height: '100%'}}
             >
                 <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+
+                <Marker 
+                    position={[-23.5184436, -51.6860508]}
+                    icon={MapIcon}
+                >
+                    <Popup closeButton={false} minWidth={240} maxWidth={240} className="map-popup">
+                        Orfanato
+                        <Link to="/orphanages/1">
+                            <FiArrowRight size={20} color="#FFF"/>
+                        </Link>
+                    </Popup>
+                </Marker>    
+                
+
             </Map>
 
-            <Link to="" className="create-orphanage">
+            <Link to="/orphanages/create" className="create-orphanage">
                 <FiPlus size={32} color="#FFF" />
             </Link>
 
